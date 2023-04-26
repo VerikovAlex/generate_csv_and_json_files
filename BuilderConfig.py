bcf69967a82c3264585c5f24dc99927ac2c263a6
protei <verikov@protei.ru>
2023-04-26 12:13:23 +0300

    change version that was made with Serega save own version for history

---
+++ BuilderConfig.py
@@ -0,0 +1,131 @@
+import csv
+import os
+import sys
+import random
+import json
+import uuid
+from typing import Dict, Tuple
+
+from pydantic import BaseModel
+
+# Описание через аннотацию типов конфиги для генерации
+class BuilderConfig(BaseModel):
+    number_of_abonents: int = 1000
+    mcc: str = '250'
+    mnc: str = '07'
+    prefix_number: str = '7911'
+    full_len_of_number: int = 11
+    realm: str = 'ims.protei.ru'
+    server_name: str = 'scscf1.ims.protei.ru'
+    capability: list = [1, 2, 3, 4]
+    probability_capability: float = 0.1
+    output_json_folder = './json_files/generated'
+    output_csv_folder = './csv_files/generated'
+
+# Функция проверки используемых для генерации параметров
+    def validate_params(self):
+        if len(self.mcc) != 3:
+            print('MCC содержит некорректное число символов. Должно быть 3 цифры')
+            sys.exit()
+        if 2 > len(self.mnc) > 3 or len(self.mnc) < 2:
+            print('MNC содержит некорректное число символов. Должно быть 2 или 3 цифры')
+            sys.exit()
+        if 15 - len(self.mcc) - len(self.mnc) < self.full_len_of_number - len(self.prefix_number):
+            print('Слишком много абонентов для генерации. Длины MSIN не хватает')
+            sys.exit()
+        if self.full_len_of_number - len(self.prefix_number) >= self.full_len_of_number:
+            print('Префикс не может быть равен или быть длиннее самого номера')
+            sys.exit()
+        if self.probability_capability > 1:
+            print('Вероятность выставления Capability не может быть больше 1')
+            sys.exit()
+        print('Параметры для генератора валидны')
+
+
+class Builder:
+
+    @staticmethod
+    def get_config():
+        return BuilderConfig()
+
+# Метод генерирования профиля абонента, получаем для конкретного абонента словарь с описанием его профиля
+    @staticmethod
+    def __generate_abonent(serial_number, config: BuilderConfig) -> Tuple[str, Dict]:
+        r = {}
+        phone_number = str(
+            config.prefix_number + str(serial_number).zfill(config.full_len_of_number - len(config.prefix_number)))
+        r['IMPI'] = str(
+            config.mcc + config.mnc + str(serial_number).zfill(15 - len(config.mcc) - len(config.mnc)))
+        r['realm'] = config.realm
+        r['capability'] = config.capability if random.random() < config.probability_capability else None
+        if not r['capability']:
+            r['servername'] = config.server_name
+        return phone_number, r
+
+# Метод, вызывающий генерирование абонентов и заполняющий csv и json файлы с ними
+    @staticmethod
+    def build(config: BuilderConfig, csv_enabled: bool = True, json_enabled: bool = True):
+        config.validate_params()
+        filename_csv = None
+        filename_json = None
+        if csv_enabled:
+            filename_csv = str(uuid.uuid4()) + '.csv'
+            if not os.path.exists(config.output_csv_folder):
+                os.makedirs(config.output_csv_folder)
+            with open(os.path.join(config.output_csv_folder, filename_csv), mode='w') as f:
+                csv_writer = csv.writer(f, delimiter=';', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+                for serial in range(1, config.number_of_abonents + 1):
+                    phone_number, abonent = Builder.__generate_abonent(serial, config)
+                    csv_writer.writerow([abonent['IMPI'], phone_number, abonent['realm']])
+
+        if json_enabled:
+            filename_json = str(uuid.uuid4()) + '.json'
+            if not os.path.exists(config.output_json_folder):
+                os.makedirs(config.output_json_folder)
+            with open(os.path.join(config.output_json_folder, filename_json), mode='w+') as f:
+                result = {}
+                for serial in range(1, config.number_of_abonents + 1):
+                    phone_number, abonent = Builder.__generate_abonent(serial, config)
+                    result[phone_number] = abonent
+                json.dump(result, f)
+
+        if filename_csv and filename_json:
+            print(filename_csv, filename_json)
+            return filename_csv, filename_json
+        if filename_csv:
+            print(filename_csv)
+            return filename_csv
+        else:
+            print(filename_json)
+            return filename_json
+
+    # Метод, вызывающий генерирование абонентов и заполняющий только json
+    @staticmethod
+    def build_json(config: BuilderConfig):
+        return Builder.build(config=config, json_enabled=True, csv_enabled=False)
+
+    # Метод, вызывающий генерирование абонентов и заполняющий только csv
+    @staticmethod
+    def build_csv(config: BuilderConfig):
+        return Builder.build(config=config, json_enabled=False, csv_enabled=True)
+
+
+if __name__ == "__main__":
+    config = BuilderConfig()
+    config.number_of_abonents = 2000
+    config.mcc = '480'
+    config.mnc = '01'
+    config.prefix_number='8981'
+    config.realm = 'abracadabra.protei.ru'
+    config.server_name = 'scscf1.abracadabra.protei.ru'
+    config.capability = [1, 2, 3, 4, 5]
+    config.probability_capability = 0.5
+    Builder.build(config)
+#    Builder.build_csv(config)
+#    Builder.build_json(config)
+
+    # config.number_of_abonents = 1_000_000
+    # from timeit import timeit
+    #
+    # time_taken = timeit("Builder.build_csv(config)", globals=globals(), number=1)
+    # print(f'{time_taken:0.6f} seconds')
--- initial_own_version.py
+++ initial_own_version.py
@@ -7,19 +7,24 @@ import json
 
 class GeneratorOfAbonents:
 
-    def __init__(self):
+    def __init__(self, mcc='250', mnc='07', prefix_number='7911', full_len_of_number=11, realm='ims.protei.ru',
+                 number_of_abonents=1000):
         self._generated_csv = './csv_files/generated/'
         self._generated_json = './json_files/generated/'
         if not os.path.exists(self._generated_csv): os.makedirs(self._generated_csv)
         if not os.path.exists(self._generated_json): os.makedirs(self._generated_json)
+        self.prefix_number: str = prefix_number
+        self.full_len_of_number: int = full_len_of_number
+        self.realm: str = realm
+        self.number_of_abonents: int = number_of_abonents
 
-    def __calculate_len_msin(self, mcc, mnc): # Вычисляем длину MSIN
+    def __calculate_len_msin(self, mcc, mnc):
         self.len_msin = 15 - len(mcc) - len(mnc)
 
-    def __calculate_len_generic_number(self, full_len_of_number, prefix_number): # Вычисляем длину генерируемой части номера
-        self.len_generic_number = full_len_of_number - len(prefix_number)
+    def __calculate_len_generic_number(self, full_len_of_number):
+        self.len_generic_number = full_len_of_number - len(self.prefix_number)
 
-    def __check_validate_args(self, mcc, mnc, prefix_number, full_len_of_number, number_of_abonents, probability_server_name, probability_capability):
+    def __check_validate_args(self, mcc, mnc, prefix_number, full_len_of_number, number_of_abonents):  # Почему нельзя сделать self для всех аргументов?
         if len(mcc) != 3:
             print('MCC содержит некорректное число символов. Должно быть 3 цифры')
             sys.exit()
@@ -35,25 +40,14 @@ class GeneratorOfAbonents:
         if self.len_generic_number < len(str(number_of_abonents)):
             print('Слишком много абонентов для генерации. Длины генерируемого номера не хватает для заданного числа абонентов')
             sys.exit()
-        if probability_server_name + probability_capability != 1:
-            print('Сумма вероятностей выставления Server Name и Capability не равна 1')
-            sys.exit()
-        if probability_server_name > 1:
-            print('Веротяность выставления Server Name не может быть больше 1')
-            sys.exit()
-        if probability_capability > 1:
-            print('Веротяность выставления Capability не может быть больше 1')
-            sys.exit()
         print('Параметры для генератора валидны')
         return None
 
     def generate_csv_file(self, mcc: str = '250', mnc: str = '07', prefix_number: str = '7911', full_len_of_number: int = 11,
-                          realm: str = 'ims.protei.ru', number_of_abonents:int = 1000, server_name: str = "scscf1.ims.protei.ru",
-                           capability: list = [1, 2, 3, 4, 5], probability_server_name: float = 0.9, probability_capability: float = 0.1,
-                           output_file_name_csv=None):
+                          realm: str = 'ims.protei.ru', number_of_abonents:int = 1000,  output_file_name_csv=None):
         self.__calculate_len_msin(mcc, mnc)
-        self.__calculate_len_generic_number(full_len_of_number, prefix_number)
-        self.__check_validate_args(mcc, mnc, prefix_number, full_len_of_number, number_of_abonents, probability_server_name, probability_capability)
+        self.__calculate_len_generic_number(full_len_of_number)
+        self.__check_validate_args(mcc, mnc, prefix_number, full_len_of_number, number_of_abonents)
         if output_file_name_csv is None:
             output_file_name_csv = str(random.randrange(0, 1000000000000, 1)) + '.csv'
         output_file_csv = self._generated_csv + output_file_name_csv
@@ -70,54 +64,48 @@ class GeneratorOfAbonents:
                 abonents_csv_writer.writerow([IMPI, userpart, realm])
         return output_file_csv
 
-    def __discription_of_specific_abonent(self, serial_number, mcc, mnc, realm, server_name, capability, probability_server_name, probability_capability):
+    def __discription_of_specific_abonent(self, serial_number, mcc, mnc, realm):
         self.dict_of_abon = dict()
         generate_MSIN = str(serial_number).zfill(self.len_msin)
         IMPI = int(mcc + mnc + generate_MSIN)
         self.dict_of_abon["IMPI"] = IMPI
         self.dict_of_abon["realm"] = realm
-        random_number_to_make_decision = random.randint(0, 100)/100
-        if random_number_to_make_decision <= probability_server_name:
-            self.dict_of_abon["servername"] = server_name
-        else:
-            self.dict_of_abon["capability"] = capability
 
     def generate_json_file(self, mcc: str = '250', mnc: str = '07', prefix_number: str = '7911', full_len_of_number: int = 11,
-                          realm: str = 'ims.protei.ru', number_of_abonents:int = 1000, server_name: str = "scscf1.ims.protei.ru",
-                           capability: list = [1, 2, 3, 4, 5], probability_server_name: float = 0.9, probability_capability: float = 0.1,
-                           output_file_name_json=None):
+                          realm: str = 'ims.protei.ru', number_of_abonents:int = 1000,  output_file_name_json=None):
         self.__calculate_len_msin(mcc, mnc)
-        self.__calculate_len_generic_number(full_len_of_number, prefix_number)
-        self.__check_validate_args(mcc, mnc, prefix_number, full_len_of_number, number_of_abonents, probability_server_name, probability_capability)
+        self.__calculate_len_generic_number(full_len_of_number)
+        self.__check_validate_args(mcc, mnc, prefix_number, full_len_of_number, number_of_abonents)
         if output_file_name_json is None:
             output_file_name_json = str(random.randrange(0, 1000000000000, 1)) + '.json'
         output_file_json = self._generated_json + output_file_name_json
         dictionary_abonents = dict()
         for serial_number in range(1, number_of_abonents + 1):
-            self.__discription_of_specific_abonent(serial_number, mcc, mnc, realm, server_name, capability, probability_server_name, probability_capability)
+            self.__discription_of_specific_abonent(serial_number, mcc, mnc, realm)
             dictionary_abonents[serial_number] = self.dict_of_abon
         with open(output_file_json, mode='w') as json_file:
             json.dump(dictionary_abonents, json_file)
         return output_file_json
 
     def generate_csv_and_json_file(self, mcc: str = '250', mnc: str = '07', prefix_number: str = '7911', full_len_of_number: int = 11,
-                          realm: str = 'ims.protei.ru', number_of_abonents:int = 1000, server_name: str = "scscf1.ims.protei.ru",
-                           capability: list = [1, 2, 3, 4, 5], probability_server_name: float = 0.9, probability_capability: float = 0.1,
-                           output_file_name_csv=None,  output_file_name_json=None):
+                          realm: str = 'ims.protei.ru', number_of_abonents:int = 1000,  output_file_name_csv=None,  output_file_name_json=None):
         self.__calculate_len_msin(mcc, mnc)
-        self.__calculate_len_generic_number(full_len_of_number, prefix_number)
-        self.__check_validate_args(mcc, mnc, prefix_number, full_len_of_number, number_of_abonents, probability_server_name, probability_capability)
+        self.__calculate_len_generic_number(full_len_of_number)
+        self.__check_validate_args(mcc, mnc, prefix_number, full_len_of_number, number_of_abonents)
         if output_file_name_csv is None:
-            output_file_name_csv = str(random.randrange(0, 1000000000000, 1)) + '.csv' # Генерируем имя сsv файла
+            output_file_name_csv = str(random.randrange(0, 1000000000000, 1)) + '.csv'
         output_file_csv = self._generated_csv + output_file_name_csv
         if output_file_name_json is None:
-            output_file_name_json = str(random.randrange(0, 1000000000000, 1)) + '.json' # Генерируем имя json файла
+            output_file_name_json = str(random.randrange(0, 1000000000000, 1)) + '.json'
         output_file_json = self._generated_json + output_file_name_json
-        dictionary_abonents = dict() # Будущий словарь для сгенерированных абоненетов, который запишем в json
+        dictionary_abonents = dict()
         with open(output_file_csv, mode='w') as csv_file:
             abonents_csv_writer = csv.writer(csv_file, delimiter=';', quotechar='"', quoting=csv.QUOTE_MINIMAL)
             for serial_number in range(1, number_of_abonents + 1):
-                #собираем IMPI абонента
+                # описываем параметры для абонента и вносим в словарь
+                self.__discription_of_specific_abonent(serial_number, mcc, mnc, realm)
+                dictionary_abonents[serial_number] = self.dict_of_abon
+                #собираем IMPI
                 generate_MSIN = str(serial_number).zfill(self.len_msin)
                 IMPI = str(mcc + mnc + generate_MSIN)
                 #собираем номер абонента
@@ -125,10 +113,6 @@ class GeneratorOfAbonents:
                 userpart = str(prefix_number + generate_number)
                 #записываем все в одну строку csv
                 abonents_csv_writer.writerow([IMPI, userpart, realm])
-                # генерим параметры для абонента и вносим в словарь
-                self.__discription_of_specific_abonent(serial_number, mcc, mnc, realm, server_name, capability,
-                                                       probability_server_name, probability_capability)
-                dictionary_abonents[userpart] = self.dict_of_abon # Записываем в словарь параметры абонента
         with open(output_file_json, mode='w') as json_file:
             json.dump(dictionary_abonents, json_file)
         return output_file_csv, output_file_json
@@ -136,9 +120,9 @@ class GeneratorOfAbonents:
 
 if __name__ == "__main__":
     list_abonents = GeneratorOfAbonents()
-    list_abonents.generate_csv_file()
-    list_abonents.generate_json_file()
-#    list_abonents.generate_csv_and_json_file()
+#    list_abonents.generate_csv_file()
+#    list_abonents.generate_json_file()
+    list_abonents.generate_csv_and_json_file()
 
 
 
